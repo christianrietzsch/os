caa6f49d6107c0ea4b6f88f1c79cfde0a980e51f
christianrietzsch <rietzsch.christian@gmail.com>
2024-04-08 11:19:26 +0200

    adds types.h

--- libc/descriptors/idt.c
+++ libc/descriptors/idt.c
@@ -1,7 +1,5 @@
 #include "../io/io.h"
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned long uint32_t;
+#include "../types/types.h"
 
 struct idt_entry {
   uint16_t base_lo;
