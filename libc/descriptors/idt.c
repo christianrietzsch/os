a900bbfb6a8f21981cbf2d1c83c124fe085d7240
christianrietzsch <rietzsch.christian@gmail.com>
2024-04-19 21:00:25 +0200

    refactors file

--- libc/descriptors/idt.c
+++ libc/descriptors/idt.c
@@ -1,9 +1,28 @@
 #include "../io/io.h"
 #include "../types/types.h"
 #include "../io/keyboard.h"
-#include "cpu.h"
 
 #define IDT_ENTRIES 256
+
+struct cpu_state 
+  {
+    uint32_t eax;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+
+    uint32_t intr;
+    uint32_t error;
+
+    uint32_t eip;
+    uint32_t cs;
+    uint32_t eflags;
+    uint32_t esp;
+    uint32_t ss;
+  };
 
 extern void intr_stub_0(void);
 extern void intr_stub_1(void);
